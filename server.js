48856dde9eeaeaa31508bd027793c949d0a1c222
Marc-Antoine <marcantoine.leonelli@gmail.com>
2023-10-21 12:25:53 +0200

    fix

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const cors = require("cors");
+require('dotenv').config();
 
 const app = express();
 const server = http.createServer(app);
@@ -46,12 +47,14 @@ io.on("connection", (socket) => {
       return;
     }
 
+    console.log("check result", player1.choice, player2.choice)
+
     if (player1.choice === player2.choice) {
       game.result = "draw";
     } else if (
-      (player1.choice === "rock" && player2.choice === "scissors") ||
+      (player1.choice === "rock" && player2.choice === "scissor") ||
       (player1.choice === "paper" && player2.choice === "rock") ||
-      (player1.choice === "scissors" && player2.choice === "paper")
+      (player1.choice === "scissor" && player2.choice === "paper")
     ) {
       game.result = player1.id;
       player1.score++;
@@ -69,118 +72,148 @@ io.on("connection", (socket) => {
     return games[gameId]?.players.find((p) => p.id === user.id)
   }
 
+  function checkGame() {
+    const game = games[gameId];
+    if (!game) {
+      socket.emit("game not found");
+      throw new Error("Game not found");
+    }
+  }
+
+  function handleError(error) {
+    console.error(error);
+    socket.emit("error", error.message);
+  }
+
   socket.on("ping", () => {
     console.log("get user", getGameUser())
   });
 
   socket.on("connect user", (id, name) => {
-    console.log("connect user", id, name)
-    if (!id) {
-      id = generateUserId();
+    try {
+      console.log("connect user", id, name)
+      if (!id) {
+        id = generateUserId();
+      }
+      if (!name) {
+        name = `Player-${id}`
+      }
+      user = {
+        id,
+        name
+      }
+      socket.emit("connected", user);
+    } catch (error) {
+      handleError(error);
     }
-    if (!name) {
-      name = `Player-${id}`
-    } 
-    user = {
-      id,
-      name
-    }
-    socket.emit("connected", user);
   })
 
   socket.on("create game", () => {
-    console.log("user", user)
-    if (!user.id || !user.name) {
-      socket.emit("user not found");
-      return;
+    try {
+      console.log("user", user)
+      if (!user.id || !user.name) {
+        socket.emit("user not found");
+        return;
+      }
+      gameId = generateGameId();
+      games[gameId] = {
+        players: [
+          {
+            id: user.id,
+            name: user.name,
+            score: 0,
+            choice: null,
+            role: "host",
+            socketId: socket.id,
+          }
+        ],
+        result: null,
+      };
+      socket.join(gameId);
+      socket.emit("game created", gameId);
+    } catch (error) {
+      handleError(error);
     }
-    gameId = generateGameId();
-    games[gameId] = {
-      players: [
-        {
+  });
+
+  socket.on("join game", (newGameId) => {
+    try {
+      const game = games[newGameId];
+      gameId = newGameId;
+
+      checkGame();
+
+      const player = getGameUser();
+      if (!player) {
+        if (game.players.length >= 2) {
+          socket.emit("game full");
+          return;
+        }
+        game.players.push({
           id: user.id,
           name: user.name,
           score: 0,
           choice: null,
-          role: "host",
+          role: "guest",
           socketId: socket.id,
-        }
-      ],
-      result: null,
-    };
-    socket.join(gameId);
-    socket.emit("game created", gameId);
-  });
-
-  socket.on("join game", (newGameId) => {
-    const game = games[newGameId];
-    gameId = newGameId;
-
-    if (!game) {
-      socket.emit("game not found");
-      return;
-    }
-    const player = getGameUser();
-    if (!player) {
-      if (game.players.length >= 2) {
-        socket.emit("game full");
-        return;
+        });
+      } else {
+        player.socketId = socket.id;
       }
-      game.players.push({
-        id: user.id,
-        name: user.name,
-        score: 0,
-        choice: null,
-        role: "guest",
-        socketId: socket.id,
-      });
-    } else {
-      player.socketId = socket.id;
+
+      socket.join(gameId);
+      socket.emit("game joined", games[gameId]);
+      sendGameUpdate();
+    } catch (error) {
+      handleError(error);
     }
-    
-    socket.join(gameId);
-    socket.emit("game joined", games[gameId]);
-    sendGameUpdate();
   });
 
   socket.on("set choice", (choice) => {
-    const game = games[gameId];
-    if (!game) {
-      socket.emit("game not found");
-      return;
-    }
-    getGameUser().choice = choice;
-    if (choice) {
-      checkResult();
+    try {
+      checkGame();
+
+      getGameUser().choice = choice;
+      if (choice) {
+        checkResult();
+      }
+    } catch (error) {
+      handleError(error);
     }
   });
 
   socket.on("continue", () => {
-    console.log("continue")
-    const game = games[gameId];
-    if (!game) {
-      socket.emit("game not found");
-      return;
+    try {
+      console.log("continue")
+      const game = games[gameId];
+
+      checkGame();
+
+      game.result = null;
+      game.players.forEach(player => {
+        player.choice = null;
+      });
+      sendGameUpdate();
+    } catch (error) {
+      handleError(error);
     }
-    game.result = null;
-    game.players.forEach(player => {
-      player.choice = null;
-    });
-    sendGameUpdate();
   });
 
   socket.on("reset", () => {
-    const game = games[gameId];
-    if (!game) {
-      socket.emit("game not found");
-      return;
+    try {
+      const game = games[gameId];
+
+      checkGame();
+
+      game.result = null;
+      game.players.forEach(player => {
+        player.choice = null;
+        player.score = 0;
+      });
+      sendGameUpdate();
+    } catch (error) {
+      handleError(error);
     }
-    game.result = null;
-    game.players.forEach(player => {
-      player.choice = null;
-      player.score = 0;
-    });
-    sendGameUpdate();
   });
 });
 
@@ -193,7 +226,7 @@ function generateUserId() {
   return Math.random().toString(16).slice(2, 10).toUpperCase();
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4545;
 server.listen(PORT, () => {
   console.log(`Serveur en écoute sur le port ${PORT}`);
 });
